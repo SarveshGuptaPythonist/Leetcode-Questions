c35fe39a851fc16181428f188534263f8ea628a4
SarveshGuptaPythonist <61269101+SarveshGuptaPythonist@users.noreply.github.com>
2021-12-05 10:01:06 +0530

    Update Find Transition Point.java

--- GeeksForGeeks/Find Transition Point.java
+++ GeeksForGeeks/Find Transition Point.java
@@ -13,4 +13,19 @@ class GfG {
         if (count == n) return -1;
         return count;
     }
+    int transitionPoint(int arr[], int n) {
+        int left = 0; int right = n-1;
+            int ans = -1;
+            while(left <= right) {
+                int mid = left + (right-left) / 2;
+                if(arr[mid] == 0) {
+                    left = mid+1;
+                }
+                else{
+                    ans = mid;
+                    right = mid-1;
+                }
+            }
+            return ans;
+    }
 }
