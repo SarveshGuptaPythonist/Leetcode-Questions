da7ced29a635dbb5023cf07e9120871e22206005
SarveshGuptaPythonist <61269101+SarveshGuptaPythonist@users.noreply.github.com>
2021-12-05 12:12:42 +0530

    Create 35. Search Insert Position.java

---
+++ 35. Search Insert Position.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        // corner case
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        return -1;
+    }
+    
+    public int searchInsert(int[] nums, int target) {
+        int ans = search(nums, target);
+        if (ans == -1){
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] > target) right = mid - 1;
+                else left = mid + 1;
+            }
+            return left;
+            
+            
+        }
+        return ans;
+    }
+
+}
