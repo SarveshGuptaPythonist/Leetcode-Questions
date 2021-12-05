f9576689f7ad3dc43344e89ffec697ed1aa1c67c
SarveshGuptaPythonist <61269101+SarveshGuptaPythonist@users.noreply.github.com>
2021-12-05 12:17:07 +0530

    Update 35. Search Insert Position.java

--- 35. Search Insert Position.java
+++ 35. Search Insert Position.java
@@ -1,3 +1,4 @@
+// dont find if exist, else again give a call
 class Solution {
     public int search(int[] nums, int target) {
         // corner case
@@ -30,4 +31,19 @@ class Solution {
         return ans;
     }
 
+}
+// Returns Left if target not found
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        return left;
+    }
 }
