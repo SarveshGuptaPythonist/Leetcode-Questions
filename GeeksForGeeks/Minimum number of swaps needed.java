c39781cbbf55bce07d2534fecf75560a38c9cedb
SarveshGuptaPythonist <61269101+SarveshGuptaPythonist@users.noreply.github.com>
2021-12-19 12:07:44 +0530

    Update Minimum number of swaps needed.java

--- GeeksForGeeks/Minimum number of swaps needed.java
+++ GeeksForGeeks/Minimum number of swaps needed.java
@@ -42,3 +42,10 @@ public static long mergeSort(long[] nums, int left, int right){
         }
         return invCount;
     }
+
+// There is an interesting solution to this problem. It can be solved using the fact that number of swaps needed is equal to number of inversions. 
+// So we basically need to count inversions in array.
+// The fact can be established using below observations: 
+// 1) A sorted array has no inversions. 
+// 2) An adjacent swap can reduce one inversion. Doing x adjacent swaps can reduce x inversions in an array. 
+ 
