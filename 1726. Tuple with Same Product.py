ffbae88c6eff59f20d7394239c22254dfa54a776
SarveshGuptaPythonist <61269101+SarveshGuptaPythonist@users.noreply.github.com>
2022-01-02 18:48:18 +0530

    Update 1726. Tuple with Same Product.py

--- 1726. Tuple with Same Product.py
+++ 1726. Tuple with Same Product.py
@@ -17,3 +17,23 @@ class Solution:
                 count += len(dict_[k]) * (len(dict_[k])-2)
         
         return count
+
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        dict_ = {}
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i!=j:
+                    prod = nums[i] * nums[j]
+                    if prod not in dict_:
+                        dict_[prod] = 0
+                    dict_[prod]+=1
+        count = 0
+        list_ = []
+        count = 0
+        for k in dict_:
+            if dict_[k] > 2:
+                list_.append([k, dict_[k]])
+                count += dict_[k] * (dict_[k]-2)
+        
+        return count
