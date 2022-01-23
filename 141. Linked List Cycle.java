64fe9763057e4c3b3c1b35f53d6deffb01d4e215
SarveshGuptaPythonist <61269101+SarveshGuptaPythonist@users.noreply.github.com>
2022-01-23 09:53:37 +0530

    Create 141. Linked List Cycle.java

---
+++ 141. Linked List Cycle.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null || head.next.next == null) return false;
+        
+        
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+        while(fast != null && fast.next != null && slow != fast) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return (slow == fast);
+    }
+}
