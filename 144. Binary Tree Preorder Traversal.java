f00a30025814a0413ee450b72472058af184485a
SarveshGuptaPythonist <61269101+SarveshGuptaPythonist@users.noreply.github.com>
2022-02-12 17:53:43 +0530

    Create 144. Binary Tree Preorder Traversal.java

---
+++ 144. Binary Tree Preorder Traversal.java
@@ -0,0 +1,15 @@
+class Solution {
+    List<Integer> res;
+    public void preOrder(TreeNode root) {
+        if(root == null) return;
+        res.add(root.val);
+        preOrder(root.left);
+        preOrder(root.right);
+        
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        res = new ArrayList();
+        preOrder(root);
+        return res;
+    }
+}
