ce8134854befaa94e7bc7004f2dcf5e9ac003459
SarveshGuptaPythonist <61269101+SarveshGuptaPythonist@users.noreply.github.com>
2022-02-19 17:17:45 +0530

    Update 117. Populating Next Right Pointers in Each Node II.java

--- 117. Populating Next Right Pointers in Each Node II.java
+++ 117. Populating Next Right Pointers in Each Node II.java
@@ -22,3 +22,43 @@ class Solution {
         return root;
     }
 }
+// this is using no extra space
+class Solution {
+    public Node connect(Node root) {
+        Node curr = root;
+        while(curr != null) {
+            Node head = null, tail = null;
+            
+            while(curr != null) {
+                
+                if(curr.left != null) {
+                    if(head == null) {
+                        head = tail = curr.left;
+                        
+                    }
+                    else{ 
+                        tail.next = curr.left;
+                        tail = tail.next;
+                    }
+                }
+                if(curr.right != null) {
+                    
+                    if(head == null) {
+                        head = tail = curr.right;
+                        
+                    }
+                    else{ 
+                        tail.next = curr.right;
+                        tail = tail.next;
+                    }
+                }
+                
+                curr = curr.next;
+            }
+            
+            curr = head;
+        }
+        return root;
+        
+    }
+}
