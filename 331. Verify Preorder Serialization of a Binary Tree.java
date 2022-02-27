49de88ef39210871b53df3bcb985e5317686d1a7
SarveshGuptaPythonist <61269101+SarveshGuptaPythonist@users.noreply.github.com>
2022-02-27 16:50:18 +0530

    Create 331. Verify Preorder Serialization of a Binary Tree.java

---
+++ 331. Verify Preorder Serialization of a Binary Tree.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isValidSerialization(String preorder) {
+        if(preorder.equals("#") == true) return true;
+        Stack<Character> stk = new Stack();// it will have only 2 values l, r
+        String[] tokens = preorder.split(",");
+        boolean root = false;
+        for(String token: tokens) {
+            if(token.equals("#") == true) {
+                if(stk.size() == 0) return false;
+                else if(stk.peek() == 'l') {
+                    stk.pop();
+                    stk.push('r');
+                }
+                else{
+                    stk.pop();
+                }
+            }
+            else {
+                if(stk.size() == 0) {
+                    if(root == false) root = true;
+                    else return false;
+                }
+                else{
+                    if(stk.peek() == 'l') {
+                        stk.pop();
+                        stk.push('r');
+                    }
+                    else{
+                        stk.pop();
+                    }
+                    
+                }
+                stk.push('l');
+            }
+        }
+        if(stk.size() == 0) return true;
+        return false;
+    }
+}
