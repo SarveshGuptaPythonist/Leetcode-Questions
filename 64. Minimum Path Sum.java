6844f512c2d0b5ba26e382366738b6ef8744e07b
SarveshGuptaPythonist <61269101+SarveshGuptaPythonist@users.noreply.github.com>
2022-05-15 10:12:59 +0530

    Create 64. Minimum Path Sum.java

---
+++ 64. Minimum Path Sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[][] memo = new int[m][n];
+
+        return helper(0, 0, m, n, grid, memo);
+    }
+    
+    public int helper(int r, int c, int m, int n, int[][] grid, int[][] memo) {
+        if(r == m || c == n) {
+            return Integer.MAX_VALUE;
+        }
+        if(r == m-1 && c == n-1) return grid[r][c];
+        if(memo[r][c] != 0) {
+            return memo[r][c];
+        }
+        
+        return memo[r][c] = grid[r][c] + Math.min(helper(r + 1, c, m, n, grid, memo), helper(r, c + 1, m, n, grid, memo));
+    }
+}
