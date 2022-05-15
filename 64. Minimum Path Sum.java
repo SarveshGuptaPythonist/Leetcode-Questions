12fa0ea9f19d6a010258f29cf34702b69e80fad0
SarveshGuptaPythonist <61269101+SarveshGuptaPythonist@users.noreply.github.com>
2022-05-15 10:43:36 +0530

    Update 64. Minimum Path Sum.java

--- 64. Minimum Path Sum.java
+++ 64. Minimum Path Sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    
+    // memoization
     public int minPathSum(int[][] grid) {
         int m = grid.length, n = grid[0].length;
         int[][] memo = new int[m][n];
@@ -17,5 +17,25 @@ class Solution {
         }
         
         return memo[r][c] = grid[r][c] + Math.min(helper(r + 1, c, m, n, grid, memo), helper(r, c + 1, m, n, grid, memo));
+    }
+    // tabulation
+    public int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        // tabulation
+        int[][] dp = new int[m][n];
+        dp[m-1][n-1] = grid[m-1][n-1];
+        for(int i = m-1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                if(i == m-1  && j == n - 1){
+                    continue;
+                }
+                dp[i][j] = Math.min(
+                    (i + 1 == m ? Integer.MAX_VALUE:dp[i+1][j]), 
+                    (j + 1 == n ? Integer.MAX_VALUE:dp[i][j+1])
+                ) + grid[i][j];
+                
+            }
+        }
+        return dp[0][0];
     }
 }
